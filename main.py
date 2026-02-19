23f5a0dc7c5cff37608a3ee169a23a48673b0f68
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2026-02-19 13:31:34 +0200

    deploid

--- main.py
+++ main.py
@@ -730,8 +730,8 @@ def run_full_pipeline(
 
 if __name__ == "__main__":
     run_full_pipeline(
-        limit_subcategories=5,
-        limit_parts_detail=4,
+        limit_subcategories=None,
+        limit_parts_detail=None,
         sniff_seconds=20,
         headless_subcategories=True,
         headless_products=True,
